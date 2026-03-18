fcfe342144f724839eb2cb1b495b2a03e73198cf
Ahmad Raza <razarajpoot99598@gmail.com>
2026-03-18 22:12:50 +0500

    routing practice

--- src/app.js
+++ src/app.js
@@ -1,26 +1,20 @@
 const express = require('express')
 
 const app = express()
-    let data = [{
-        name:'user1',
-        age:20,
-        city:"lahore"
-    },
-    {
-        name:'user2',
-        age:21,
-        city:"Lahore"
-    }
-]
 
-app.use("/data", (req, res)=>{
-
-    res.send(JSON.stringify(data))
+app.use("/user", (req, res, next)=>{
+    console.log("hanlder 1")
+    //res.send("Response 1")
+    next()
+}, (req, res, next)=>{
+    next()
+    console.log("Hanlder 2")
+    res.send("Response 2")
+    
+}, (req, res)=>{
+    console.log("handler 3")
 })
-
-app.use("/", (req, res)=>{
-    res.send("DevConnect Inc.")
-})
+    
 
 app.listen(3000, ()=>{
     console.log('server is running on port: 3000')
