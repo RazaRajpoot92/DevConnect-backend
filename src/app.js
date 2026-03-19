5d7cb9ca341790a9b4b59fa74d04bcd3ce0fdf5a
Ahmad Raza <razarajpoot99598@gmail.com>
2026-03-19 22:43:32 +0500

    error handling

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use("/admin", adminAuth)
 
 
 app.get("/admin/sales", (req, res)=>{
-
+    throw new Error()
     res.status(200).send("Sales Data")
 })
 
@@ -15,7 +15,11 @@ app.get("/admin/sales/analytics", (req, res)=>{
     res.status(200).send("sales analytics data")
 })
 
-    
+app.use('/', (err, req, res, next)=>{
+    if(err){
+        res.status(500).send("Something went wrong!")
+    }
+})
 
 app.listen(3000, ()=>{
     console.log('server is running on port: 3000')
