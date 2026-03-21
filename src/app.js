dc3f4369a7172c260dc5ffa2bb167cb66c63feed
Ahmad Raza <razarajpoot99598@gmail.com>
2026-03-22 00:23:43 +0500

    started with signup api

--- src/app.js
+++ src/app.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const connectDB = require("./config/database.js")
+const User = require("./models/user.js")
 const app = express()
+
+
+// signup api for creating user in database thorugh usermodel
+
+app.post("/signup", async(req, res)=>{
+    const userData = {
+        firstName:"Ahmad",
+        lastName:"Raza",
+        age:25,
+        gender:"male",
+        email:"raza@gmail.com",
+        password:"029384092"
+    }
+
+    try{
+        const user = new User(userData)
+        
+        await user.save()
+
+        res.send("User has been created!")
+
+    }catch(err){
+        res.send(400).send("Error while saving.. Error:", err.message)
+    }
+
+})
 
 
 connectDB().then(()=>{
