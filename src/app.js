a87188ccd2636e41e6c9fffd725216588c7176e7
Ahmad Raza <razarajpoot99598@gmail.com>
2026-03-29 15:04:06 +0500

    created connection requeset system

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser")
 const {userAuth} = require("./middlewares/auth.js")
 const authRouter = require('./routers/authRouter.js')
 const profileRouter = require('./routers/profileRouter.js')
+const connectionRouter = require('./routers/connectionRouter.js')
 
 const app = express()
 
@@ -13,79 +14,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(authRouter)
 app.use(profileRouter)
-
-// signup api for creating user in database thorugh usermodel
-
-// create user
-
-
-// app.get("/profile", userAuth, async (req, res)=>{
-    
-//     try{
-        
-//         const user = req.user
-       
-//         res.status(200).json({
-//             "success":true,
-//             "user data":user,
-//         })
-       
-//     }catch(error){
-//         res.status(400).json({
-//             "success":false,
-//             "message":"Invalid request"
-//         })
-//     }
-
-// })
-
-// get user by id
-app.get("/user/:id", async (req, res)=>{
-    const userEmail = req.params.id
-    
-
-    try{
-        const user = await User.findById(userEmail)
-
-        res.send(user)
-
-    }catch(err){
-        res.status(400).send("something went wrong..")
-    }
-})
-
-// update user (Patch)
-app.patch("/user", async (req, res)=>{
-    const userId = req.body.id
-    //console.log(userId)
-    const newUser = req.body 
-
-    try{
-        const allowedUpdate = ["id","gender", "firstName", "lastName", "photoUrl"]
-        const isAllowedUpdate = Object.keys(newUser).every((k)=> allowedUpdate.includes(k))
-
-        if(!isAllowedUpdate){
-            throw new Error("Update not allowed")
-        }
-
-        const user = await User.findOneAndUpdate({_id: userId}, newUser, {runValidators:true})
-       // console.log(user)
-        res.send("User has been updated!")
-    }catch(err){
-        res.status(400).send("Something went wrong.."+ err.message)
-    }
-})
-
-app.delete("/user",  async (req, res)=>{
-    const id = req.body.id
-
-    try{
-        const data = await User.findOneAndDelete({_id: id})
-        res.send(`User has been deleted ${data}`)
-    }catch(err){
-        res.send("something went wrong.")
-    }
-})
+app.use(connectionRouter)
 
 
 connectDB().then(()=>{
---
+++ src/routers/connectionRouter.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const {userAuth} = require('../middlewares/auth')
+const ConnectionRequest = require('../models/connectionRequest')
+const User = require('../models/user')
+
+const connectionRouter = express.Router()
+
+
+connectionRouter.post('/request/send/:status/:touserid', userAuth, async (req,res)=>{
+    try{
+        const toUserId = req.params.touserid
+        const fromUserId = req.user._id
+        const status = req.params.status
+
+        const user = await User.findById(toUserId)
+
+        if(!user){
+            return res.status(404).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+
+        const allowedStatus = ["interested","ignored"]
+        const isValidStatus = allowedStatus.includes(status)
+
+        if(!isValidStatus){
+            return res.status(400).json({
+                success:false,
+                message: `${status} is not valid status type`
+            })
+        }
+
+        const isConnectionAlreadyExist = await ConnectionRequest.findOne({
+            $or:[
+                {fromUserId, toUserId },
+                {fromUserId: toUserId, toUserId:fromUserId}
+            ]
+        })
+
+        if(isConnectionAlreadyExist){
+            return res.status(400).json({
+                success:false,
+                message:"Connection Request Already Exists"
+            })
+        }
+
+
+        const newConnectionRequest = new ConnectionRequest({
+            toUserId,
+            fromUserId,
+            status
+        })
+
+        await newConnectionRequest.save()
+
+        res.status(201).json({
+            success:true,
+            message:"Connection Request has been sent!"
+        })
+
+
+    }catch(error){
+        res.status(400).json({
+            success:false,
+            message:error.message
+        })
+    }
+
+})
+
+
+module.exports = connectionRouter
