3c344324d54f5cc136e7e70658c5e6e41fc6aef6
Ahmad Raza <razarajpoot99598@gmail.com>
2026-04-06 18:24:04 +0500

    built recived connections request and get all connections apis

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -4,6 +4,8 @@ const ConnectionRequest = require('../models/connectionRequest')
 
 const userRouter = express.Router()
 
+const USER_SAFE_DATA = ["firstName", "lastName", "age","gender", "photoUrl", "skills"]
+
 userRouter.get('/user/requests/recieved', userAuth, async (req, res)=>{
     try{
 
@@ -12,8 +14,8 @@ userRouter.get('/user/requests/recieved', userAuth, async (req, res)=>{
         const connectionRequests = await ConnectionRequest.find({
             toUserId:loggedInUser._id,
             status:"interested"
-        })
-//lksjD@99fl1
+        }).populate("fromUserId", USER_SAFE_DATA)
+
         if(!connectionRequests){
             return res.status(404).json({
                 success:false,
@@ -25,6 +27,49 @@ userRouter.get('/user/requests/recieved', userAuth, async (req, res)=>{
             success:true,
             data:connectionRequests
         })
+
+    }catch(error){
+        return res.status(400).json({
+            success:false,
+            message:error.message
+        })
+    }
+})
+
+
+userRouter.get("/user/connections", userAuth, async (req, res)=>{
+    try{
+        const loggedInUser = req.user
+
+        const connections = await ConnectionRequest.find({
+            $or:[
+                {fromUserId: loggedInUser, status:"accepted"},
+                {toUserId:loggedInUser, status:"accepted"}
+            ]
+        }).populate("fromUserId", USER_SAFE_DATA).populate("toUserId", USER_SAFE_DATA)
+
+        
+        if(!connections){
+            return res.status(404).json({
+                success:false,
+                message:"Not found"
+            })
+        }
+
+        const data = connections.map((conn) => {
+            if(conn.fromUserId._id.toString() == loggedInUser._id.toString()){
+                return conn.toUserId
+            }
+
+            return conn.fromUserId
+        })
+
+        return res.status(200).json({
+            success:true,
+            data,
+        
+        })
+
 
     }catch(error){
         return res.status(400).json({
