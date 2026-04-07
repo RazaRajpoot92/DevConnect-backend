80390876eb1761e8d5b2d39d4b45be5a973ada49
Ahmad Raza <razarajpoot99598@gmail.com>
2026-04-07 17:54:01 +0500

    built feed api

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const { userAuth } = require('../middlewares/auth')
 const ConnectionRequest = require('../models/connectionRequest')
+const User = require('../models/user')
 
 const userRouter = express.Router()
 
@@ -75,6 +76,46 @@ userRouter.get("/user/connections", userAuth, async (req, res)=>{
         return res.status(400).json({
             success:false,
             message:error.message
+        })
+    }
+})
+
+userRouter.get('/user/feed', userAuth, async(req, res)=>{
+
+    try{
+        const loggedInUser = req.user
+
+        const connections = await ConnectionRequest.find({
+            $or:[
+                {toUserId:loggedInUser._id},
+                {fromUserId:loggedInUser._id}
+            ]
+        }).select("toUserId fromUserId")
+
+        const hideUserFromFeed = new Set()
+
+        connections.forEach(connection =>{
+            hideUserFromFeed.add(connection.fromUserId)
+            hideUserFromFeed.add(connection.toUserId)
+        })
+
+        const users = await User.find({
+            $and:[
+                {_id: {$nin: Array.from(hideUserFromFeed)}},
+                {_id: {$ne: loggedInUser._id}}
+            ]
+        }).select(USER_SAFE_DATA)
+
+        return res.status(200).json({
+            success:true,
+            data:users
+        })
+
+
+    }catch(error){
+        return res.status(400).json({
+            success:false,
+            message: error.message
         })
     }
 })
